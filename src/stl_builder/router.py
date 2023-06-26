4519f7b47ff90e6126896e97b31a533d88c332a5
Nep Nep <compsci88@gmail.com>
2023-06-25 19:00:58 -0700

    add v1

--- src/stl_builder/router.py
+++ src/stl_builder/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .service import write_container_stl
 
 
-router = APIRouter()
+router = APIRouter(prefix='/v1')
 
 
 @router.get("/")
@@ -14,3 +14,8 @@ async def homepage() -> str:
 @router.get("/3d-box")
 async def demo_get():
     return {"url": write_container_stl(50, 50, 50, 1)}
+
+
+@router.post('/shapes')
+async def create_shapes():
+    ...
